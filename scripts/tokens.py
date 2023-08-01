aa9e930a2f0f9fb5f9eb959f012ed89976d2080a
thepigcat <benpospo@gmail.com>
2023-08-01 23:02:20 +0200

    testing scripts

--- scripts/tokens.py
+++ scripts/tokens.py
@@ -1,6 +1,15 @@
 file_path = "src/tokens.rs"
 
-with open(file_path, 'a') as file:
+with open(file_path, 'r') as file:
     # Use the print() function to write to the file
-    print("Hello, this is a sample content.", file=file)
-    print("This is another line.", file=file)
+    file_content = file.read()
+
+for l in range(len(file_content) - 1):
+    if file_content[l] == "T":
+        identifier = ""
+        while not file_content[l].isspace():
+            identifier += file_content[l]
+            print(identifier)
+            l += 1
+        for i in range(9):
+            print(file_content[l + i], end="")
