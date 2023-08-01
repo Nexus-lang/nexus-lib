a8917d1f96f50b24f05aeabaa59fa98f01db2f27
thepigcat <benpospo@gmail.com>
2023-08-01 19:54:38 +0200

    proper utf-8 checking

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,6 +6,7 @@ macro_rules! push_token {
     };
 }
 
+//TODO: the issue with characters in front of other characters in identifiers not being read needs to be fixed again
 pub fn lex(input: String) -> Vec<Token> {
     let input_chars: Vec<char> = input.chars().collect();
     let mut tokens: Vec<Token> = Vec::new();
@@ -14,33 +15,52 @@ pub fn lex(input: String) -> Vec<Token> {
     while current_pos < input_chars.len() {
         let ch = input_chars[current_pos];
 
-        if !ch.is_whitespace() {
-            let mut identifier = String::new();
-            identifier.push(ch);
+        match ch {
+            c if c.is_alphabetic() || is_utf8_and_not_ascii(c)=> {
+                let mut identifier = String::new();
+                identifier.push(ch);
 
-            let mut next_pos = current_pos + 1;
-            while next_pos < input_chars.len() && !input_chars[next_pos].is_whitespace() {
-                identifier.push(input_chars[next_pos]);
-                next_pos += 1;
+                let mut next_pos = current_pos + 1;
+                while next_pos < input_chars.len() && (input_chars[next_pos].is_alphanumeric() || is_utf8_and_not_ascii(input_chars[next_pos])) {
+                    identifier.push(input_chars[next_pos]);
+                    next_pos += 1;
+                }
+
+                current_pos = next_pos;
+
+                match identifier.as_str() {
+                    i if i == TokenType::VAR.literal() => {
+                        push_token!(tokens, TokenType::VAR)
+                    }
+                    i if i == TokenType::ASSIGN.literal() => {
+                        push_token!(tokens, TokenType::ASSIGN)
+                    }
+                    _ => {
+                        tokens.push(Token(TokenType::IDENT, identifier));
+                    }
+                }
             }
-
-            current_pos = next_pos;
-
-            match identifier.as_str() {
-                i if i == TokenType::VAR.literal() => {
-                    push_token!(tokens, TokenType::VAR)
-                }
-                i if i == TokenType::ASSIGN.literal() => {
-                    push_token!(tokens, TokenType::ASSIGN)
-                }
-                _ => {
-                    tokens.push(Token(TokenType::IDENT, identifier));
-                }
+            _ => {
+                println!("uwu");
+                current_pos += 1;
             }
-        } else {
-            println!("uwu");
-            current_pos += 1;
         }
     }
     tokens
+}
+
+fn is_utf8_and_not_ascii(c: char) -> bool {
+    let s = c.to_string();
+    let bytes = s.as_bytes();
+
+    // Check if the bytes form a valid UTF-8 string
+    
+    if !s.is_ascii() {
+        if let Ok(_) = std::str::from_utf8(bytes) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    false
 }
--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,61 +1,20 @@
-#[derive(Debug)]
-enum TokenType {
-    HELLO,
-    WORLD,
-    IDENT,
-}
-
-impl TokenType {
-    fn literal(&self) -> &'static str {
-        match self {
-            Self::HELLO => "Hello",
-            Self::WORLD => "World",
-            Self::IDENT => "IDENT",
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Token(pub TokenType, pub String);
-
 fn main() {
-    let test: Vec<char> = "Hello World 🦀".chars().collect();
-    let mut tokens: Vec<Token> = Vec::new();
-    println!("{:?}", test);
+    let test = '你';
+    println!("{}", is_utf8_and_not_ascii(test))
+}
 
-    let mut current_pos = 0;
-    while current_pos < test.len() {
-        let ch = test[current_pos];
+fn is_utf8_and_not_ascii(c: char) -> bool {
+    let s = c.to_string();
+    let bytes = s.as_bytes();
 
-        if !ch.is_whitespace() {
-            let mut identifier = String::new();
-            identifier.push(ch);
-
-            let mut next_pos = current_pos + 1;
-            while next_pos < test.len() && !test[next_pos].is_whitespace() {
-                identifier.push(test[next_pos]);
-                next_pos += 1;
-            }
-
-            current_pos = next_pos;
-
-            match identifier.as_str() {
-                i if i == TokenType::HELLO.literal() => {
-                    tokens.push(Token(TokenType::HELLO, TokenType::HELLO.literal().to_string()))
-                }
-                i if i == TokenType::WORLD.literal() => {
-                    tokens.push(Token(TokenType::WORLD, TokenType::WORLD.literal().to_string()))
-                }
-                _ => {
-                    tokens.push(Token(TokenType::IDENT, identifier));
-                }
-            }
+    // Check if the bytes form a valid UTF-8 string
+    
+    if !s.is_ascii() {
+        if let Ok(_) = std::str::from_utf8(bytes) {
+            return true;
         } else {
-            println!("uwu");
-            current_pos += 1;
+            return false;
         }
     }
-
-    println!("{:?}", tokens);
-    println!("\"")
+    false
 }
