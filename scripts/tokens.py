6b92658724cbb4267d957997b9821dacdc426c4b
thepigcat <benpospo@gmail.com>
2023-08-01 22:50:25 +0200

    started working on script to add tokens

---
+++ scripts/tokens.py
@@ -0,0 +1,6 @@
+file_path = "src/tokens.rs"
+
+with open(file_path, 'a') as file:
+    # Use the print() function to write to the file
+    print("Hello, this is a sample content.", file=file)
+    print("This is another line.", file=file)
