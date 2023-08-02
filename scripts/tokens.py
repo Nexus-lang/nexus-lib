73371f0334e480ba1c748289d758bd9aa5986bdb
thepigcat <benpospo@gmail.com>
2023-08-02 11:52:06 +0200

    changed it to read from the file instead of using string literal

--- scripts/tokens.py
+++ scripts/tokens.py
@@ -1,71 +1,7 @@
 import re
 
-rust_enum = """
-#[derive(Debug)]
-pub enum TokenType {
-    // Keywords
-    VAR,
-    CONST,
-    FUNC,
-    FOR,
-    WHILE,
-    IF,
-    ELSE,
-    WHEN,
-    USE,
-
-
-    // Other keywords
-    
-
-    // Special character
-    LCURLY,       // {
-    RCURLY,       // }
-    LPARENT,     // (
-    RPARENT,     // )
-    LSQUAREBRAC, // [
-    RSQUAREBRAC, // ]
-    COMMENT,     // //
-    QUOTMARK,   // "
-    EXCLAMMARK,
-    COMMA,       // ,
-    COLON,       // :
-
-    // Identifier, literals
-    IDENT,
-    NUMBER,
-    FALSE,
-    TRUE,
-
-    // Other
-    ILLEGAL, // illeagl expression
-    EOL,     // End of line
-
-    // Arithmetic operators and alike
-    PLUS,        // +
-    MINUS,       // -
-    DIVIDE,      // /
-    MULTIPLY,    // *
-    ASSIGN,      // =
-
-    EQUAL,       // ==
-    NOTEQUAL,    // !=
-    GREATERTHAN, // >
-    LESSERTHAN,    // <
-    GREATEROREQUALTHAN, // >=
-    LESSEROREQUALTHAN,    // <=
-
-    AND,
-    OR,
-    IN,
-    ARROW, // ->
-    OTHER,
-
-    // Structures
-    STRUCT,
-    ENUM,
-}
-"""
+with open("src/tokens.rs") as file:
+    rust_enum = file.read()
 
 # Use regular expression to find the entire enum block
 enum_block = re.search(r'enum TokenType \{([\s\S]*?)\n\}', rust_enum).group(1)
