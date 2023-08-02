a5f840d0f98ce07f52d2df0915fb775590dfb7d9
thepigcat <benpospo@gmail.com>
2023-08-02 16:43:11 +0200

    reimplemented lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -39,10 +39,136 @@ pub fn lex(input: String) -> Vec<Token> {
 
                 match identifier {
                     i if i == TokenType::VAR.literal() => {
-                        push_token!(tokens, TokenType::VAR)
+                        push_token!(tokens, TokenType::VAR);
+                    }
+                    i if i == TokenType::CONST.literal() => {
+                        push_token!(tokens, TokenType::CONST)
+                    }
+                    i if i == TokenType::IF.literal() => {
+                        push_token!(tokens, TokenType::IF);
+                    }
+                    i if i == TokenType::ELSE.literal() => {
+                        push_token!(tokens, TokenType::ELSE);
+                    }
+                    i if i == TokenType::FOR.literal() => {
+                        push_token!(tokens, TokenType::FOR);
+                    }
+                    i if i == TokenType::WHILE.literal() => {
+                        push_token!(tokens, TokenType::WHILE);
+                    }
+                    i if i == TokenType::FUNC.literal() => {
+                        push_token!(tokens, TokenType::FUNC);
+                    }
+                    i if i == TokenType::WHEN.literal() => {
+                        push_token!(tokens, TokenType::WHEN);
+                    }
+                    i if i == TokenType::USE.literal() => {
+                        push_token!(tokens, TokenType::USE);
+                    }
+                    i if i == TokenType::OTHER.literal() => {
+                        push_token!(tokens, TokenType::OTHER);
+                    }
+                    i if i == TokenType::IN.literal() => {
+                        push_token!(tokens, TokenType::IN);
+                    }
+
+                    // Literals
+                    i if i == TokenType::TRUE.literal() => {
+                        push_token!(tokens, TokenType::TRUE);
+                    }
+                    i if i == TokenType::FALSE.literal() => {
+                        push_token!(tokens, TokenType::FALSE);
+                    }
+
+                    // Comparison
+                    i if i == TokenType::AND.literal() => {
+                        push_token!(tokens, TokenType::AND);
+                    }
+                    i if i == TokenType::OR.literal() => {
+                        push_token!(tokens, TokenType::OR);
+                    }
+
+                    // Data Structures
+                    i if i == TokenType::STRUCT.literal() => {
+                        push_token!(tokens, TokenType::STRUCT);
+                    }
+                    i if i == TokenType::ENUM.literal() => {
+                        push_token!(tokens, TokenType::ENUM);
+                    }
+                    i if i == TokenType::LCURLY.literal() => {
+                        push_token!(tokens, TokenType::LCURLY);
+                    }
+                    i if i == TokenType::RCURLY.literal() => {
+                        push_token!(tokens, TokenType::RCURLY);
+                    }
+                    i if i == TokenType::LPARENT.literal() => {
+                        push_token!(tokens, TokenType::LPARENT);
+                    }
+                    i if i == TokenType::RPARENT.literal() => {
+                        push_token!(tokens, TokenType::RPARENT);
+                    }
+                    i if i == TokenType::LSQUAREBRAC.literal() => {
+                        push_token!(tokens, TokenType::LSQUAREBRAC);
+                    }
+                    i if i == TokenType::RSQUAREBRAC.literal() => {
+                        push_token!(tokens, TokenType::RSQUAREBRAC);
+                    }
+
+                    // Arithmetic operations
+                    i if i == TokenType::PLUS.literal() => {
+                        push_token!(tokens, TokenType::PLUS);
+                    }
+                    i if i == TokenType::MINUS.literal() => {
+                        push_token!(tokens, TokenType::MINUS);
+                    }
+                    i if i == TokenType::MULTIPLY.literal() => {
+                        push_token!(tokens, TokenType::MULTIPLY);
+                    }
+                    i if i == TokenType::DIVIDE.literal() => {
+                        push_token!(tokens, TokenType::DIVIDE);
                     }
                     i if i == TokenType::ASSIGN.literal() => {
-                        push_token!(tokens, TokenType::ASSIGN)
+                        push_token!(tokens, TokenType::ASSIGN);
+                    }
+
+                    // Comparison
+                    i if i == TokenType::EQUAL.literal() => {
+                        push_token!(tokens, TokenType::EQUAL);
+                    }
+                    i if i == TokenType::NOTEQUAL.literal() => {
+                        push_token!(tokens, TokenType::NOTEQUAL);
+                    }
+                    i if i == TokenType::GREATERTHAN.literal() => {
+                        push_token!(tokens, TokenType::GREATERTHAN);
+                    }
+                    i if i == TokenType::GREATEROREQUALTHAN.literal() => {
+                        push_token!(tokens, TokenType::GREATEROREQUALTHAN);
+                    }
+                    i if i == TokenType::LESSERTHAN.literal() => {
+                        push_token!(tokens, TokenType::LESSERTHAN);
+                    }
+                    i if i == TokenType::LESSEROREQUALTHAN.literal() => {
+                        push_token!(tokens, TokenType::LESSEROREQUALTHAN);
+                    }
+
+                    // Misc types
+                    i if i == TokenType::QUOTMARK.literal() => {
+                        push_token!(tokens, TokenType::QUOTMARK);
+                    }
+                    i if i == TokenType::COMMENT.literal() => {
+                        push_token!(tokens, TokenType::COMMENT);
+                    }
+                    i if i == TokenType::COMMA.literal() => {
+                        push_token!(tokens, TokenType::COMMA);
+                    }
+                    i if i == TokenType::COLON.literal() => {
+                        push_token!(tokens, TokenType::COLON);
+                    }
+                    i if i == TokenType::ARROW.literal() => {
+                        push_token!(tokens, TokenType::ARROW);
+                    }
+                    i if i == TokenType::EXCLAMMARK.literal() => {
+                        push_token!(tokens, TokenType::EXCLAMMARK)
                     }
                     _ => {
                         tokens.push(Token(TokenType::IDENT, identifier));
