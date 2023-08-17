0dcf5d188fb9259fbf68686a74ac44fa37667b62
thepigcat <benpospo@gmail.com>
2023-08-17 20:04:49 +0200

    remove commented-out code

--- src/lexer.rs
+++ src/lexer.rs
@@ -327,20 +327,3 @@ impl Lexer {
         tokens
     }
 }
-/*
-fn is_utf8_and_not_ascii(c: char) -> bool {
-    let s = c.to_string();
-    let bytes = s.as_bytes();
-
-    // Check if the bytes form a valid UTF-8 string
-
-    if !s.is_ascii() {
-        if let Ok(_) = std::str::from_utf8(bytes) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    false
-}
-*/
