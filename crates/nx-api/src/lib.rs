d75cf18712992379679d486d4fd847578e2c7041
thepigcat <benpospo@gmail.com>
2024-01-19 19:43:14 +0100

    rename

---
+++ crates/nx-api/src/lib.rs
@@ -0,0 +1,21 @@
+use evaluator::Evaluator;
+use lexer::Lexer;
+use parser::Parser;
+
+pub fn execute_program(path: &str) {
+    // TODO: Update clutils to allow string slices for paths in filehandler
+    let mut lexer = Lexer::new(&path.into()).expect(&format!(
+        "Failed to open file at {}. Maybe the file path is incorrect",
+        path
+    ));
+    let mut parser = Parser::new(&mut lexer);
+    let mut evalutator = Evaluator::new();
+    loop {
+        let stmt = match parser.parse_stmt() {
+            Ok(stmt) => stmt,
+            Err(_) => break,
+        };
+        evalutator.eval_stmt(stmt);
+        parser.next_token();
+    }
+}
